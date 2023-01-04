f5ee9d4c940658ee43940a09fa7bbe6ff74ae13c
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-05 01:58:36 +0300

    fix bug

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include "Point.h"
 
-double distance(Point a, Point b)
+double distance(Point &a, Point &b)
 {
     return sqrt(pow(static_cast<double>(a.get_x() - b.get_x()), 2) 
                 + pow(static_cast<double>(a.get_y() - b.get_y()), 2));
