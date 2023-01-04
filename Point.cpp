75c5106c5a29fc7cb24bd66b126318d38feadfed
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-04 22:49:29 +0300

    start my features

--- Point.cpp
+++ Point.cpp
@@ -1,1 +1,12 @@
 // Class implementation
+#include "Point.h"
+
+void Point::set_x(int _x)
+{
+    x = _x >= 0 ? _x : 0;
+}
+
+void Point::set_y(int _y)
+{
+    y = _y >= 0 ? _y : 0;
+}
--- Point.h
+++ Point.h
@@ -7,8 +7,11 @@ class Point
 private:
     int x, y;
 public:
-    int get_x(){return x;} // inline function
+    int get_x(){return x;} // default inline function
     int get_y(){return y;}
+
+    void set_x(int);
+    void set_y(int);
 };
 
 #endif
