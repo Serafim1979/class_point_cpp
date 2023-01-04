dd7715d1d6989e8472038e854d9e73ba772331b7
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-04 21:55:48 +0300

    start development

---
+++ Point.h
@@ -0,0 +1,14 @@
+// Class defenition
+#ifndef POINT_H
+#define POINT_H
+
+class Point
+{
+private:
+    int x, y;
+public:
+    int get_x(){return x;} // inline function
+    int get_y(){return y;}
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Point.h"
+
+int main()
+{
+    Point a;
+    return 0;
+}
