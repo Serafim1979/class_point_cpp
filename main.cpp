9cb5276de37ba3a092aeb81f44f213c306449699
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-05 01:31:51 +0300

    Added fuction distance

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cmath>
 #include "Point.h"
+
+double distance(Point a, Point b)
+{
+    return sqrt(pow(static_cast<double>(a.get_x() - b.get_x()), 2) 
+                + pow(static_cast<double>(a.get_y() - b.get_y()), 2));
+}
 
 int main()
 {
-    Point a;
+    Point a, b;
     return 0;
 }
